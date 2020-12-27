68d3ec2b048bc05f9678b1c4fb6dbb836ea9720e
zeroistfilm <zeroistfilm@naver.com>
2020-12-27 20:24:13 +0900

    init

--- 영동/01_DFS와 BFS.py
+++ 영동/01_DFS와 BFS.py
@@ -1,1 +1,41 @@
-sdasdasddssfdaasdfdsfsdfasdafadsf
+# https://www.acmicpc.net/problem/1260
+
+N, M, V = map(int, input().split())
+
+
+adjacencymatrix = [[0 for i in range(N)] for i in range(N)]
+adjacencyList = [[] for i in range(N)]
+for i in range(M):
+    nodes = list(map(int, input().split()))
+    #인접행렬만들
+    adjacencymatrix[nodes[0] - 1][nodes[1] - 1] = 1
+    adjacencymatrix[nodes[1] - 1][nodes[0] - 1] = 1
+    #인접리스트 만들기
+    adjacencyList[nodes[1] - 1].append(nodes[0]-1)
+    adjacencyList[nodes[0] - 1].append(nodes[1]-1)
+
+
+def dfs(graph,v,visited):
+    visited[v] = True
+    print(v+1, end=' ')
+    for i in sorted(graph[v]):
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+def bfs(graph,v,visited):
+    from collections import deque
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        a=queue.popleft()
+        print(a+1,end=' ')
+        for i in sorted(graph[a]):
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
+
+visited = [False for i in range(N)]
+dfs(adjacencyList,V-1,visited)
+print()
+visited2 = [False for i in range(N)]
+bfs(adjacencyList,V-1,visited2)
---
+++ 영동/02_바이러스.py
@@ -0,0 +1,29 @@
+# https://www.acmicpc.net/problem/2606
+
+N = int(input()) #컴퓨터 수
+edge = int(input())
+
+adlist = [[] for i in range(N)]
+for i in range(edge):
+    x, y = map(int, input().split())
+    adlist[x - 1].append(y - 1)
+    adlist[y - 1].append(x - 1)
+
+infected=0
+def bfs(graph,start,visited,infected):
+    from collections import deque
+    queue=deque([start])
+    visited[start]=True
+    while queue:
+        v=queue.popleft()
+        #print(v+1, end=' ')
+        infected+=1
+        for i in sorted(graph[v]):
+            if not visited[i]:
+                queue.append(i)
+                visited[i]=True
+    return infected
+
+visited=[False for i in range(N)]
+infected=bfs(adlist,0,visited,infected)
+print(infected-1)
