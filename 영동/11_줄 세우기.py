afdd48ba9207c589f6acad9baf995ffa602410c8
zeroistfilm <zeroistfilm@naver.com>
2020-12-29 14:19:49 +0900

    2차 풀이 성공

--- 영동/11_줄 세우기.py
+++ 영동/11_줄 세우기.py
@@ -3,33 +3,33 @@
 
 from collections import deque
 
-V,E = map(int ,input().split())
+v, e = map(int, input().split())
 
-approch=[[]for i in range(V)]
-link=[[]for i in range(V)]
+indegree = [0] * (v + 1)
+graph = [[]for i in range(v+1)]
 
-for i in range(E):
-    a,b  = map(int,input().split())
-    approch[b-1].append(a-1) #집입차수
-    link[a - 1].append(b - 1)  #간선 정보
+for i in range(e):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b]+=1
 
-queue = deque()
-visited=[False for i in range(V)]
-접근순서 =[]
-def topology(approch,link,visited):
-    for _ in range(V):
-        for i in range(V):#모든 노드를 돌면서 진입차수가 0인 노드를 모두 집어 넣는다
-            if len(approch[i])==0 and visited[i]==False:
-                queue.append(i) # 노드 번호를 큐에 집어 넣는다
-                접근순서.append(i)
-                visited[i]=True
+def topology_sort():
+    result=[]
+    q = deque()
+    for i in range(1, v+1):
+        if indegree[i]==0:
+            q.append(i)
 
-        while queue:
-            popv = queue.popleft()
-            c=link[popv]
-            for j in range(len(link[popv])):
-                approch[link[popv][j]]=[] #간정 정보를 비운다
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for i in graph[now]:
+            indegree[i]-=1
+            if indegree[i]==0:
+                q.append(i)
 
-topology(approch,link,visited)
-for i in range(len(접근순서)):
-    print(접근순서[i]+1,end=' ')
+
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
